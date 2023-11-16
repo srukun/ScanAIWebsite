70b1d9c5359ccb3d98d6c5571a2a956c8a1849ea
Shahriyar Rukun <sharrukun2@gmail.com>
2023-11-15 20:33:23 -0800

    Scan Button
    .

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function test(){
-    console.log("Test");
-}
+ // JavaScript
+
+ let profilePic = document.querySelector(".card img"); // Selecting the image element within the card
+ let inputFile = document.getElementById("input-file"); // Getting the file input element by its ID
+
+ inputFile.onchange = function(){
+     profilePic.src = URL.createObjectURL(inputFile.files[0]); // Changing the source of the image to the selected file
+     // TODO: send the image to the model. This will be simple and we will work on this as a team
+ }
+
